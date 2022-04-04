e93feac62153d8a65a1574eb3f6ed36a67783a9f
ThallesLana <thalles.lana1@gmail.com>
2022-04-04 11:37:10 -0300

    finalizando estilização dos botões

--- src/components/newTransactionModal/index.tsx
+++ src/components/newTransactionModal/index.tsx
@@ -34,12 +34,12 @@ export function NewTransactionModal({isOpen, onRequestClose} : NewTransactionMod
                 <input placeholder="Valor" type="number" />
 
                 <TransactionTypeContainer>
-                    <RadioBox type="button" onClick={() => {setType('deposit'); }} isActive={type === 'deposit'}>
+                    <RadioBox type="button" onClick={() => {setType('deposit'); }} isActive={type === 'deposit'} activeColor="green">
                         <img src={incomeImg} alt="Entrada"/>
                         <span>Entrada</span>
                     </RadioBox>
                     
-                    <RadioBox type="button" onClick={() => {setType('withdraw'); }} isActive={type === 'withdraw'}>
+                    <RadioBox type="button" onClick={() => {setType('withdraw'); }} isActive={type === 'withdraw'} activeColor="red">
                         <img src={outcomeImg} alt="Saída"/>
                         <span>Saída</span>
                     </RadioBox>
